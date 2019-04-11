22ff273de102fc09d4df3e75726fee79410cedbb
Tohka <mcdanieln@student.swosu.edu>
2019-04-11 15:47:41 -0500

    adding capstone info

--- Python Files/file name trial.py
+++ Python Files/file name trial.py
@@ -1,6 +1,13 @@
-from Tkinter import *
-import Tkinter, Tkconstants, tkFileDialog
+# from Tkinter import *
+# import Tkinter, Tkconstants, tkFileDialog
  
-root = Tk()
-root.filename = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-print (root.filename)
+# root = Tk()
+# root.filename = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+# print (root.filename)
+
+import xlrd
+book = xlrd.open_workbook('trial.xlsx')
+sheet = book.sheet_by_name('Sheet1')
+data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+# Profit !
+print(data)
